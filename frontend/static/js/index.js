7a253036939dff2e48cc9fc3732f250efa6ce223
Cerebrovinny <vinny.cardoso96@gmail.com>
2021-03-29 07:20:59 -0300

    Added Single page app

---
+++ frontend/static/js/index.js
@@ -0,0 +1,33 @@
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("dashboard") },
+        { path: "/posts", view: () => console.log("posts") },
+        { path: "/about-me", view: () => console.log("about") },
+    ];
+
+    //test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    console.log(potentialMatches)
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
